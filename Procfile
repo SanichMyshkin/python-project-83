3c15a2019e203e057c343153c8495488ecde1086
Александр Мышкин <sir.myshkin@gmail.com>
2023-03-23 23:03:33 +0300

    war

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn page_analyzer:app
--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -3,10 +3,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.get('/')
 def index():
     return render_template('index.html')
-
-
-if __name__ == '__main__':
-    app.run(port=5000)
