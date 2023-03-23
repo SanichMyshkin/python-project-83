6058460a56b5179b077b68140188906c798044b7
Александр Мышкин <sir.myshkin@gmail.com>
2023-03-23 22:06:38 +0300

    war

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/", methods=['POST'])
+@app.route('/')
 def index():
     return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(port=5000)
