fdbfd1b35ac71f7d4f84bf155927af9b9a991b40
Александр Мышкин <sir.myshkin@gmail.com>
2023-04-02 00:50:48 +0300

    complate 3 steps

--- page_analyzer/testing.py
+++ page_analyzer/testing.py
@@ -1,9 +1,6 @@
-from page_analyzer.validate import is_valid
 from page_analyzer.connected import connect_to_db
 
-
-
-max_query = f'''SELECT MAX(id) FROM urls'''
+max_query = '''SELECT MAX(id) FROM urls'''
 
 max_id = connect_to_db(max_query)
 print(max_id[0][0])
