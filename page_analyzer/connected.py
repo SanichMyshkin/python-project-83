a598519c4bc8c5c41101cec5bb1330e33666f06e
Александр Мышкин <sir.myshkin@gmail.com>
2023-04-13 15:51:33 +0300

    fix local varible

--- page_analyzer/connected.py
+++ page_analyzer/connected.py
@@ -9,8 +9,6 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 def connect_to_db(request, *args):
     try:
         conn = psycopg2.connect(DATABASE_URL)
-        # print('Sql connected')
-
         with conn.cursor() as cursor:
             cursor.execute(request, args)
             response = cursor.fetchall()
@@ -18,26 +16,14 @@ def connect_to_db(request, *args):
 
     except Exception as _ex:
         print('Error while working with PSQL', _ex)
-    finally:
-        if conn:
-            conn.close()
-            # print("Connection Database closed")
 
 
 def insert_to_db(request):
     try:
         conn = psycopg2.connect(DATABASE_URL)
 
-        # print("Sql connected")
-
         with conn.cursor() as cursor:
             cursor.execute(request)
-
+            conn.commit()
     except Exception as _ex:
         print('Error while working with PSQL', _ex)
-
-    finally:
-        if conn:
-            conn.commit()
-            conn.close()
-            # print("Connection close")
