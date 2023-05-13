d24386f8b1881a8e0fc8d93d6b1cda7fc3c78e24
Александр Мышкин <sir.myshkin@gmail.com>
2023-05-13 20:50:50 +0300

    fix

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,22 +1,20 @@
-from dotenv import load_dotenv
 from flask import Flask, render_template, \
     flash, request, redirect, url_for
-import os
 
+import os
 from datetime import datetime
+from dotenv import load_dotenv
+
 from page_analyzer.models import get_all_db, \
     insert_to_db, get_id
-from page_analyzer.connected import connect_to_db
+from page_analyzer.connected import get_connection
 from page_analyzer.checks_request import get_data_html, get_status
 from page_analyzer.validate import is_valid, get_normalize_domain
 
-load_dotenv()
-SECRET_KEY = os.getenv('SECRET_KEY')
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = SECRET_KEY
-
-connection = connect_to_db()
+load_dotenv()
+app.secret_key = os.getenv("SECRET_KEY")
 
 
 @app.route('/', methods=['GET'])
@@ -45,9 +43,10 @@ def get_sites():
                       ON max_checks.max_id = url_checks.id
                       ORDER BY urls.id DESC'''
 
-    responce = get_all_db(connection, query)
-    return render_template('urls.html',
-                           data=responce)
+    with get_connection() as connected:
+        responce = get_all_db(connected, query)
+        return render_template('urls.html',
+                               data=responce)
 
 
 @app.route('/urls', methods=["POST"])
@@ -57,70 +56,77 @@ def post_sites():
     url = data['url']
 
     current_url = get_normalize_domain(url)
+    with get_connection() as connection:
+        id = get_id(connection, current_url)
 
-    id = get_id(connection, current_url)
+        errors = is_valid(data)
 
-    errors = is_valid(data)
+        if errors:
+            flash(f"{errors['name']}", 'alert alert-danger')
+            return render_template("index.html",
+                                   data=current_url), 422
 
-    if errors:
-        flash(f"{errors['name']}", 'alert alert-danger')
-        return render_template("index.html",
-                               data=current_url), 422
+        if id:
+            flash('Страница уже существует', 'alert alert-info')
+            return redirect(url_for('id_sites', id=id))
 
-    if id:
-        flash('Страница уже существует', 'alert alert-info')
+        sql_query = f'''INSERT INTO urls(name, created_at)
+                        VALUES('{current_url}','{datetime.today()}')'''
+        insert_to_db(connection, sql_query)
+        flash("Страница успешно добавлена", 'alert alert-success')
+
+        query_id = f"SELECT id FROM urls WHERE name='{current_url}'"
+        id = get_all_db(connection, query_id)[0][0]
+
         return redirect(url_for('id_sites', id=id))
-
-    sql_query = f'''INSERT INTO urls(name, created_at)
-                    VALUES('{current_url}','{datetime.today()}')'''
-    insert_to_db(connection, sql_query)
-    flash("Страница успешно добавлена", 'alert alert-success')
-
-    query_id = f"SELECT id FROM urls WHERE name='{current_url}'"
-    id = get_all_db(connection, query_id)[0][0]
-
-    return redirect(url_for('id_sites', id=id))
 
 
 @app.route("/urls/<int:id>", methods=["POST", "GET"])
 def id_sites(id):
     url_id = f'''SELECT * FROM urls WHERE id={id}'''
-    data_of_url = get_all_db(connection, url_id)
+    with get_connection() as connection:
+        data_of_url = get_all_db(connection, url_id)
 
-    if not data_of_url:
-        return render_template('error.html'), 200
+        if not data_of_url:
+            return render_template('error.html'), 200
 
-    url_id_check = f'''SELECT * FROM url_checks WHERE url_id={id}
-                       ORDER BY id DESC'''
-    data_check = get_all_db(connection, url_id_check)
-    return render_template("url_id.html",
-                           id=id,
-                           data=data_of_url,
-                           checks=data_check)
+        url_id_check = f'''SELECT * FROM url_checks WHERE url_id={id}
+                           ORDER BY id DESC'''
+        data_check = get_all_db(connection, url_id_check)
+        return render_template("url_id.html",
+                               id=id,
+                               data=data_of_url,
+                               checks=data_check)
 
 
 @app.post('/urls/<int:id>/checks')
 def url_checks(id):
     query_select = f'''SELECT * FROM urls WHERE id={id}'''
-    data_url = get_all_db(connection, query_select)
-    url_id = data_url[0][0]
-    url_name = data_url[0][1]
-    url_date = datetime.today()
-    url_status_code = get_status(connection, id)
-    if url_status_code != 200:
-        flash('Произошла ошибка при проверке', 'alert alert-danger')
-        return redirect(url_for('id_sites', id=id, code=422))
+    with get_connection() as connection:
+        data_url = get_all_db(connection, query_select)
+        url_id = data_url[0][0]
+        url_name = data_url[0][1]
+        url_date = datetime.today()
+        url_status_code = get_status(connection, id)
+        if url_status_code != 200:
+            flash('Произошла ошибка при проверке', 'alert alert-danger')
+            return redirect(url_for('id_sites', id=id, code=422))
 
-    data_html = get_data_html(url_name)
+        data_html = get_data_html(url_name)
 
-    query = f'''INSERT INTO
-            url_checks(url_id, status_code, h1, title, description, created_at)
-            VALUES({url_id},
-                    {url_status_code},
-                    '{data_html["h1"]}',
-                    '{data_html["title"]}',
-                    '{data_html["description"]}',
-                    '{url_date}')'''
-    insert_to_db(connection, query)
-    flash('Страница успешно проверена', 'alert alert-success')
-    return redirect(url_for('id_sites', id=id))
+        query = f'''INSERT INTO
+                url_checks(url_id, status_code, h1, title,
+                            description, created_at)
+                VALUES({url_id},
+                        {url_status_code},
+                        '{data_html["h1"]}',
+                        '{data_html["title"]}',
+                        '{data_html["description"]}',
+                        '{url_date}')'''
+        insert_to_db(connection, query)
+        flash('Страница успешно проверена', 'alert alert-success')
+        return redirect(url_for('id_sites', id=id))
+
+
+if __name__ == '__main__':
+    app()
--- page_analyzer/connected.py
+++ page_analyzer/connected.py
@@ -1,17 +1,45 @@
 import psycopg2
-from dotenv import load_dotenv
-import psycopg2.extras
-import os
-
-load_dotenv()
-DATABASE_URL = os.getenv('DATABASE_URL')
+from psycopg2 import pool
+from page_analyzer.settings import DB_HOST, DB_PORT, \
+    DB_USER, DB_PASS, DB_NAME
+from contextlib import contextmanager
 
 
-def connect_to_db():
-    connection = None
+def create_connection(*args, **kwargs):
+    return psycopg2.connect(
+        host=DB_HOST,
+        port=DB_PORT,
+        user=DB_USER,
+        password=DB_PASS,
+        database=DB_NAME)
+
+
+def create_pool(min_conn=1, max_conn=5):
+    """Create connection for work with PostgresSQL"""
+    return pool.SimpleConnectionPool(minconn=min_conn,
+                                     maxconn=max_conn,
+                                     connection_factory=create_connection,
+                                     host=DB_HOST,
+                                     port=DB_PORT,
+                                     user=DB_USER,
+                                     password=DB_PASS,
+                                     database=DB_NAME)
+
+
+@contextmanager
+def get_connection():
+    """Get connection from pool or error if connection doesn't work"""
+    conn = None
     try:
-        connection = psycopg2.connect(DATABASE_URL)
-        connection.autocommit = True
-    except Exception as _ex:
-        print('Error while working with PSQL', _ex)
-    return connection
+        conn = conn_pool.getconn()
+        yield conn
+        conn.commit()
+    except Exception as error:
+        conn.rollback()
+        raise Exception(f'Connection lost. Changes abort. {error}')
+    finally:
+        if conn:
+            conn_pool.putconn(conn)
+
+
+conn_pool = create_pool()
